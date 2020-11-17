31b075f27824b9aac15df256bfd6b50e8d48fabc
Simon Ekizean <ekizean@gmail.com>
2020-11-17 06:14:50 +0100

    Clean up Gatsby start site

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,10 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import scrollTo from "gatsby-plugin-smoothscroll"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#111111`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,
@@ -17,17 +13,6 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
       <button onClick={() => scrollTo("#menu")}>Meny</button>
       <button onClick={() => scrollTo("#menu")}>Vinprovningar</button>
       <button onClick={() => scrollTo("#menu")}>Galleri</button>
--- src/components/layout.js
+++ src/components/layout.js
@@ -13,24 +13,12 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
