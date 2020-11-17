00fe5a1492340f1a25bc95c065b1e04a535437b3
Simon Ekizean <ekizean@gmail.com>
2020-11-17 07:43:14 +0100

    Divider complete

---
+++ src/components/divider.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./divider.css"
+import Image from "./image"
+
+const Divider = () => (
+  <div class="divider">
+    <div class="divider__line"></div>
+    <Image />
+    <div class="divider__line"></div>
+  </div>
+)
+
+export default Divider
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,16 @@ import SEO from "../components/seo"
 import About from "../components/about"
 import Hero from "../components/hero"
 import Menu from "../components/Menu/menu"
+import Divider from "../components/divider"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Brezza" />
     <Hero />
     <Menu />
+    <Divider />
     <About />
+    <Divider />
   </Layout>
 )
 
