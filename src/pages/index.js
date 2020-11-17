4feb11d98aea0bf476376c9489c1fc4ecc2c1b40
Simon Ekizean <ekizean@gmail.com>
2020-11-17 06:54:51 +0100

    Add Footer and Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import About from "../components/about"
+import Hero from "../components/hero"
 import Menu from "../components/Menu/menu"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Brezza" />
+    <Hero />
     <Menu />
     <About />
   </Layout>
