ec35017ab6c93c51cd99d65d43e33848939c1b2b
Simon Ekizean <ekizean@gmail.com>
2020-11-17 07:09:50 +0100

    Book buttons

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,27 @@
 import React from "react"
 import "./hero.css"
 import Logo from "../../svg/logo.svg"
+import { Link } from "gatsby"
 
 const Hero = () => (
   <div className="hero">
     <Logo />
+    <Link
+      class="book-button"
+      target="_blank"
+      rel="noreferrer"
+      to="https://cloud.caspeco.se/public/webBooking?isWebBooking=true&system=se_breupp&unitId=13"
+    >
+      Boka bord
+    </Link>
+
+    <Link
+      class="book-button"
+      target="_blank"
+      to="https://cloud.caspeco.se/public/webBooking?system=se_breupp&unitId=14&onlineorder=true"
+    >
+      Avhämtning 20%
+    </Link>
   </div>
 )
 export default Hero
