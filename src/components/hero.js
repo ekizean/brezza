75081be08ac9eb288a02cd839178830dfb21483c
Simon Ekizean <ekizean@gmail.com>
2020-11-17 06:13:29 +0100

    Add gatsby svg

---
+++ src/components/hero.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./hero.css"
+import Logo from "../../svg/logo.svg"
+
+const Hero = () => (
+  <div className="hero">
+    <Logo />
+  </div>
+)
+export default Hero
