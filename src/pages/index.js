4b1a999e6e6ecc8139d853c6932f3dab1f875250
Simon Ekizean <ekizean@gmail.com>
2020-11-18 06:45:00 +0100

    Add wine tasting

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,19 @@ import About from "../components/about"
 import Hero from "../components/hero"
 import Menu from "../components/Menu/menu"
 import Divider from "../components/divider"
+import WineTasting from "../components/wine-tasting"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Brezza" />
-    <Hero />
-    <Menu />
-    <Divider />
-    <About />
-    <Divider />
-  </Layout>
+    <Layout>
+        <SEO title="Brezza" />
+        <Hero />
+        <Menu />
+        <Divider />
+        <WineTasting />
+        <Divider />
+        <About />
+        <Divider />
+    </Layout>
 )
 
 export default IndexPage
