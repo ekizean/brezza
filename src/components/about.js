98eb8ee11870f2f0a3b189ce076c02e42f170718
Simon Ekizean <ekizean@gmail.com>
2020-12-04 07:36:36 +0100

    General styling (finputs)

--- src/components/about.js
+++ src/components/about.js
@@ -2,14 +2,26 @@ import React from 'react'
 
 const About = () => (
     <div id="about">
-        BREZZA betyder bris på italienska och vår tanke är att smakupplevelser ska färdas hit från
-        Italien. BREZZA är också en nödvändig faktor i vinodling för vindruvornas välbefinnande. Vi
-        är en italiensk restaurang och vinotek i centrala Uppsala med fokus på klassisk italiensk
-        matlagning med moderna influenser. Här kan du äta allt från
-        <strong>charkuterier, ostar och vedugnsbakad pizza till á la carte</strong>
-        med flera rätter på italienskt vis - eller endast ta ett glas vin från vår vinkällare. Vi är
-        mycket stolta över vår samling av viner från olika regioner av Italien, såsom Amarone,
-        Barolo, Barbaresco, Brunello, Chianti m.m. Boka gärna en vinprovning i vår vinkällare.
+        <h1 className="text-center">Om Brezza</h1>
+        <div>
+            BREZZA betyder bris på italienska och vår tanke är att smakupplevelser ska färdas hit
+            från Italien. BREZZA är också en nödvändig faktor i vinodling för vindruvornas
+            välbefinnande.
+            <br />
+            <br />
+            Vi är en italiensk restaurang och vinotek i centrala Uppsala med fokus på klassisk
+            italiensk matlagning med moderna influenser.
+            <br />
+            <br />
+            Här kan du äta allt från
+            <strong>charkuterier, ostar och vedugnsbakad pizza till á la carte</strong>
+            med flera rätter på italienskt vis - eller endast ta ett glas vin från vår vinkällare.
+            <br />
+            <br />
+            Vi är mycket stolta över vår samling av viner från olika regioner av Italien, såsom
+            Amarone, Barolo, Barbaresco, Brunello, Chianti m.m. Boka gärna en vinprovning i vår
+            vinkällare.
+        </div>
     </div>
 )
 
