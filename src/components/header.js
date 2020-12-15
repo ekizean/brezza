ae2a731f82c7a8d8468f6ab5a421b47957e8a33a
Simon Ekizean <ekizean@gmail.com>
2020-12-15 07:30:04 +0100

    Add burger for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -4,33 +4,24 @@ import scrollTo from 'gatsby-plugin-smoothscroll'
 import LogoGold from './images/logo-gold'
 import './header.scss'
 import BookButton from './book-button'
+import NavButtons from './nav-buttons'
+import Burger from './burger'
 
 const Header = ({ siteTitle }) => (
     <header>
         <div className="pointer" onClick={() => scrollTo('.hero')}>
             <LogoGold />
         </div>
-        <div className="navigator">
-            <button className="nav-button pointer" onClick={() => scrollTo('#menu')}>
-                Meny
-            </button>
-            <button className="nav-button pointer" onClick={() => scrollTo('.wine-tasting')}>
-                Vinprovningar
-            </button>
-            <button className="nav-button pointer" onClick={() => scrollTo('.gallery')}>
-                Galleri
-            </button>
-            <button className="nav-button pointer" onClick={() => scrollTo('#about')}>
-                Om&nbsp;oss
-            </button>
-            <button className="nav-button pointer" onClick={() => scrollTo('footer')}>
-                Kontakt
-            </button>
+        <div className="navigator desktop">
+            <NavButtons />
         </div>
         <BookButton
             text="Boka&nbsp;bord"
             link="https://cloud.caspeco.se/public/webBooking?isWebBooking=true&system=se_breupp&unitId=13"
         />
+        <div className="mobile burger-wrapper">
+            <Burger />
+        </div>
     </header>
 )
 
---
+++ src/components/nav-buttons.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import scrollTo from 'gatsby-plugin-smoothscroll'
+
+const NavButtons = ({ close }) => {
+    const handleClick = element => {
+        scrollTo(element)
+        close()
+    }
+
+    return (
+        <>
+            <button className="nav-button pointer" onClick={() => handleClick('#menu')}>
+                Meny
+            </button>
+            <button className="nav-button pointer" onClick={() => handleClick('.wine-tasting')}>
+                Vinprovningar
+            </button>
+            <button className="nav-button pointer" onClick={() => handleClick('.gallery')}>
+                Galleri
+            </button>
+            <button className="nav-button pointer" onClick={() => handleClick('#about')}>
+                Om&nbsp;oss
+            </button>
+            <button className="nav-button pointer" onClick={() => handleClick('footer')}>
+                Kontakt
+            </button>
+        </>
+    )
+}
+
+export default NavButtons
