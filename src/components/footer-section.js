67ec9ecd15684e44e56e5c16f06418b35ed79c40
Simon Ekizean <ekizean@gmail.com>
2020-12-16 20:50:51 +0100

    Style footer - more mobile friendly

--- src/components/footer-section.js
+++ src/components/footer-section.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './footer-section.scss'
 
 const FooterSection = ({ header, content }) => (
-    <div className="col-sm-3">
+    <div className="col-sm-3 footer-section">
         <h2>{header}</h2>
         {content}
     </div>
