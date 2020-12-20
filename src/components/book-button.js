553072b95d134253ce0958609baa5eb5a5567372
Simon Ekizean <ekizean@gmail.com>
2020-12-20 10:01:34 +0100

    Updates

--- src/components/book-button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './book-button.scss'
-
-const BookButton = ({ text, link }) => (
-    <a className="book-button" href={link}>
-        {text}
-    </a>
-)
-export default BookButton
