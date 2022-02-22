99266c8ac82e6d650240488a7574f0295c80639c
Simon Ekizean <ekizean@gmail.com>
2022-02-22 22:12:35 +0100

    Add campaign

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import About from '../components/about'
 import Hero from '../components/hero'
+import Campaign from '../components/campaign'
 import Menu from '../components/Menu/menu'
 import Divider from '../components/divider'
 import WineTasting from '../components/wine-tasting'
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <Layout>
         <SEO title="Brezza" />
         <Hero />
+        <Campaign />
         <Menu />
         <Divider />
         <WineTasting />
