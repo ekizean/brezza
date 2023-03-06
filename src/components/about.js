964dd1496f031cadf350e703c89208ea32efad64
Simon <ekizean@gmail.com>
2023-03-06 19:45:45 +0100

    About about section

--- src/components/about.js
+++ src/components/about.js
@@ -7,18 +7,27 @@ const About = () => (
             BREZZA betyder bris på italienska.
             <br />
             <br />
-            BREZZA är en italiensk restaurang och vinotek med kvalitetsviner från hela världen med fokus på italienska toppviner såsom <strong>Brunello, Chianti, Barolo, Barbaresco, Amarone, Sassicaia</strong> m.m. BREZZA har en egen vinkällare där vinprovningar anordnas.
+            BREZZA är en klassisk italiensk restaurang med moderna influenser. Vår ambition är att
+            hålla högsta kvalitet på råvaror och tillaga allt från grunden.
             <br />
             <br />
-            På BREZZA serverar vi klassisk italiensk <strong>antipasti, secondi, dolci, handgjord pasta och vedugnsbakade pizzor</strong>.
+            Här kan du äta allt från <strong>charkuterier, ostar, hemgjord pasta</strong> och{' '}
+            <strong>vedugnsbakad pizza</strong> till á la carte med flera rätter på italienskt vis -
+            eller endast ta ett glas vin.
             <br />
             <br />
-            BREZZAs anrika och ståtliga lokaler har flera olika rum och en vinkällare. Restaurangen lämpar sig väl som festsal för både små och stora evenemang och högtider såsom bröllop, familjemiddagar, företagsevent, personalfester m.m.
+            BREZZAs anrika och ståtliga lokaler har flera olika rum och en vinkällare. I vår
+            vinkällare finns ett stort urval av all världens viner men vårt fokus ligger på
+            italienska viner. Vi har bl.a.{' '}
+            <strong>Sassicaia, Masseto, Tignanello, Barolo, Amarone, Brunello</strong> m.m.
+            <br />
+            <br />
+            BREZZA är det perfekta stället för familjemiddagar, företagsevent, personalfester eller
+            en romantisk middag på tu man hand.
             <br />
             <br />
             Välkomna till BREZZA!
         </span>
-        
     </div>
 )
 
