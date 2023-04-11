a49a2c7d4c6b110f6cc2c38a42d96d008f3a9644
Simon <ekizean@gmail.com>
2023-04-11 19:30:08 +0200

    Remove campaign section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import About from '../components/about'
 import Hero from '../components/hero'
-import Campaign from '../components/campaign'
 import Menu from '../components/Menu/menu'
 import Divider from '../components/divider'
 import WineTasting from '../components/wine-tasting'
@@ -14,7 +13,6 @@ const IndexPage = () => (
     <Layout>
         <SEO title="Brezza" />
         <Hero />
-        <Campaign />
         <Menu />
         <Divider />
         <WineTasting />
