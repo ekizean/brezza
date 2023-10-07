1c4fd95d54096bc01962db6dfcde350ab11b26f7
Simon <ekizean@gmail.com>
2023-10-07 12:26:53 +0200

    Temporary october ad

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './hero.scss'
 import Logo from '../../svg/logo.svg'
-import BookButton from './book-button'
-import scrollTo from 'gatsby-plugin-smoothscroll'
+// import BookButton from './book-button'
+// import scrollTo from 'gatsby-plugin-smoothscroll'
 
 const Hero = () => (
     <div className="hero">
         <Logo />
-        <BookButton
+        {/* <BookButton
             text="Boka&nbsp;bord"
             link="https://cloud.caspeco.se/public/webBooking?isWebBooking=true&system=se_breupp&unitId=13"
         />
@@ -17,7 +17,15 @@ const Hero = () => (
         />
         <button className="book-button" onClick={() => scrollTo('.wine-tasting')}>
             <span>VINPROVNINGAR</span>
-        </button>
+        </button> */}
+        <div className="october-text">
+            <p className="ad first">Brezza fyller 5 år 🎉</p>
+            <br />
+            <p className="ad second">
+                Det firas under hela oktober med 20% på AW vardagar 16 - 17, vinprovningar onsdagar
+                och torsdagar samt hela notan på söndagar
+            </p>
+        </div>
     </div>
 )
 export default Hero
